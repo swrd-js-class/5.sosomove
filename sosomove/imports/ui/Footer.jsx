c59e217c9810d6c76e30424706822aafa38f288d
hyojung222 <sxz0905@naver.com>
2024-11-13 20:13:18 +0900

    메인페이지 완료

---
+++ sosomove/imports/ui/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      © 2024 소소이사(주)
+    </div>
+  );
+};
--- sosomove/imports/ui/Postinsert.jsx
+++ sosomove/imports/ui/Postinsert.jsx
@@ -4,5 +4,5 @@ import { useParams } from "react-router-dom";
 export default () => {
   const params = useParams();
   // params._id
-  return <div>This is detail page: {params._id}</div>;
+  return <div>This is postinsert page: {params._id}</div>;
 };
