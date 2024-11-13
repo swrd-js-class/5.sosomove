ad6585c3d474ea95b1d51b1c9478b1d9509d4c5c
Songhui <skyblue1001@naver.com>
2024-11-13 16:32:39 +0900

    ksh first commit

--- sosomove/imports/ui/App.jsx
+++ sosomove/imports/ui/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Home.jsx";
 import NotFound from "./NotFound.jsx";
 import Nav from "./Nav.jsx";
 import PostDetail from "./PostDetail.jsx";
+import DatePickerComponent from '../publicAPI/Calendar.jsx';
 
 export const App = () => (
   <Router>
@@ -16,5 +17,8 @@ export const App = () => (
       ></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
+    <div>
+      <DatePickerComponent /> {/* 달력 컴포넌트 삽입 */}
+    </div>
   </Router>
 );
