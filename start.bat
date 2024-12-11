34c4a2a826723a01facf3c597a94a74d76dd2d54
효정 <sxz0905@naver.com>
2024-12-11 16:14:25 +0900

    12/11최종

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+@REM MONGO_URL=mongodb+srv://ssong1001:songhe!0@cluster0.gwbeq.mongodb.net/?retryWrites=true^&w=majority^&appName=Cluster0
 set MONGO_URL=mongodb+srv://hyojung222:tlsgywjd7!@cluster0.t3xnv.mongodb.net/project?retryWrites=true^&w=majority^&appName=Cluster0
 set AZURE_API_KEY=8FJ4HK4kROZM2zu1yhxQe3C5sOBMZZHCFjRT8jRTvxTy5L4g4uqgJQQJ99AKACYeBjFXJ3w3AAABACOGBbVs
 set AZURE_API_URL=https://shj-pk.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview
