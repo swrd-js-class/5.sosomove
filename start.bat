f7c2986b3560dece04b16e6e646ac298a4f0411a
Songhui <skyblue1001@naver.com>
2024-12-11 16:55:40 +0900

    회원가입 시 로그아웃하며 로그인화면으로 전환

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-set MONGO_URL=mongodb+srv://hyojung222:tlsgywjd7!@cluster0.t3xnv.mongodb.net/project?retryWrites=true^&w=majority^&appName=Cluster0
+set MONGO_URL=mongodb+srv://ssong1001:songhe!0@cluster0.gwbeq.mongodb.net/?retryWrites=true^&w=majority^&appName=Cluster0
 set AZURE_API_KEY=8FJ4HK4kROZM2zu1yhxQe3C5sOBMZZHCFjRT8jRTvxTy5L4g4uqgJQQJ99AKACYeBjFXJ3w3AAABACOGBbVs
 set AZURE_API_URL=https://shj-pk.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview
 set AZURE_CV_KEY=3bCGjL1DVZWqmm28yT2y3aHTPNYkleql49sRVFF5TIKsfkiBpT1KJQQJ99ALACYeBjFXJ3w3AAAFACOGUhdT
