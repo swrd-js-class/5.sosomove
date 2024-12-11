8d725c6132a7456496382bccf640d2fb2b60bd5f
Songhui <skyblue1001@naver.com>
2024-12-11 12:10:54 +0900

    allEstimate ksh  수정

--- imports/ui/BusinessAllEstimate.jsx
+++ imports/ui/BusinessAllEstimate.jsx
@@ -188,33 +188,62 @@
 //   );
 // };
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { CollectionRequest, CollectionEstimate } from '/imports/api/collections';
 
 export default () => {
   const [statusFilter, setStatusFilter] = useState(0); // 상태 필터 (0: 전체, 1: 대기중, 2: 매칭, 3: 매칭 취소)
   const [isLoading, setIsLoading] = useState(true); // 로딩 상태
+  
+  const [estimateinReq, setEstimateinReq] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const { user, estimateinReq } = useTracker(() => {
+  useEffect(() => {
+    if (estimateinReq.length > 0) {
+      estimateinReq.forEach((req) => {
+        if (req.status === 3) {
+          setShowAlert(true);
+          return;
+        }
+      });
+    }
+  }, [estimateinReq]);
+
+  // const { user, estimateinReq } = useTracker(() => {
+  const { user } = useTracker(() => {
     const estimateSub = Meteor.subscribe('CollectionEstimate');
     const requestSub = Meteor.subscribe('CollectionRequest');
 
+    // if (!estimateSub.ready() || !requestSub.ready()) {
+    //   return { user: null, estimateinReq: [] };
+    // }
     if (!estimateSub.ready() || !requestSub.ready()) {
-      return { user: null, estimateinReq: [] };
+      return { user: null };
     }
 
     const user = Meteor.user();
     const businessId = user._id;
     const estimates = CollectionEstimate.find({ business_id: businessId }).fetch();
 
-    const estimateinReq = estimates.map((estimate) => ({
+    // const estimateinReq = estimates.map((estimate) => ({
+    //   ...estimate,
+    //   request: CollectionRequest.findOne({ _id: estimate.request_id }),
+    // }));
+    const estimateinReqRequest = estimates.map((estimate) => ({
       ...estimate,
       request: CollectionRequest.findOne({ _id: estimate.request_id }),
     }));
 
+    // // 날짜 오름차순 정렬
+    // estimateinReq.sort((a, b) => {
+    //   const dateA = new Date(a.request?.move_date || 0);
+    //   const dateB = new Date(b.request?.move_date || 0);
+    //   return dateA - dateB;
+    // });
+
     // 날짜 오름차순 정렬
-    estimateinReq.sort((a, b) => {
+    estimateinReqRequest.sort((a, b) => {
       const dateA = new Date(a.request?.move_date || 0);
       const dateB = new Date(b.request?.move_date || 0);
       return dateA - dateB;
@@ -222,8 +251,10 @@ export default () => {
 
     // 데이터가 준비되었으므로 로딩 상태를 false로 설정
     setIsLoading(false);
+    setEstimateinReq(estimateinReqRequest);
 
-    return { user, estimateinReq };
+    // return { user, estimateinReq };
+    return { user };
   }, []);
 
   // 상태 필터 적용
@@ -242,6 +273,13 @@ export default () => {
   if (!estimateinReq.length) {
     return <p className="text-gray-500">견적서를 찾을 수 없습니다.</p>;
   }
+
+
+
+  // 알림 닫기
+  const closeAlert = () => {
+    setShowAlert(false);
+  };
 
   const handleDelete = (estimateId) => {
     Meteor.call('estimate.delete', estimateId, (err) => {
@@ -282,110 +320,123 @@ export default () => {
   };
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="mt-6 text-base font-semibold text-gray-900">
-            {user.profile.name}님이 제출한 견적서 전체 목록
-          </h1>
+    <>
+      {showAlert && (
+        <div className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-green-400 to-blue-500 text-white p-4 rounded-lg shadow-lg text-xl flex items-center justify-between z-50">
+          <span>취소된 견적서가 있습니다!</span>
+          <button
+            onClick={closeAlert}
+            className="ml-4 bg-white text-black rounded-full p-2 hover:bg-gray-200 transition">
+            닫기
+          </button>
         </div>
-        {/* 상태 필터 */}
-        <div className="sm:ml-4 mt-4 sm:mt-0">
-          <select
-            value={statusFilter}
-            onChange={(e) => setStatusFilter(Number(e.target.value))}
-            className="mt-4 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-            <option value={0}>전체</option>
-            <option value={1}>대기중</option>
-            <option value={2}>매칭</option>
-            <option value={3}>매칭 취소</option>
-          </select>
+      )}
+
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="mt-6 text-base font-semibold text-gray-900">
+              {user.profile.name}님이 제출한 견적서 전체 목록
+            </h1>
+          </div>
+          {/* 상태 필터 */}
+          <div className="sm:ml-4 mt-4 sm:mt-0">
+            <select
+              value={statusFilter}
+              onChange={(e) => setStatusFilter(Number(e.target.value))}
+              className="mt-4 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option value={0}>전체</option>
+              <option value={1}>대기중</option>
+              <option value={2}>매칭</option>
+              <option value={3}>매칭 취소</option>
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">요청자</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">이사 날짜</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">출발지</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">도착지</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">상세 내용</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">금액</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">상태</th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span className="sr-only">삭제</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {filteredEstimates.length > 0 ? (
-                    filteredEstimates.map((estimateReq) => (
-                      <tr key={estimateReq._id}>
-                        {estimateReq.request && (
-                          <>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6">
-                              {estimateReq.request.user_name || 'N/A'}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
-                              {estimateReq.request.move_date
-                                ? new Date(estimateReq.request.move_date).toLocaleDateString()
-                                : 'N/A'}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
-                              {estimateReq.request.start_address || 'N/A'}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
-                              {estimateReq.request.arrive_address || 'N/A'}
-                            </td>
-                          </>
-                        )}
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
-                          {estimateReq.details || 'N/A'}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
-                          {estimateReq.amount ? `${estimateReq.amount}원` : 'N/A'}
-                        </td>
-                        <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                          <span className="relative inline-block px-3 py-1 font-semibold leading-tight">
-                            <span
-                              aria-hidden="true"
-                              className={`absolute inset-0 ${getStatusStyle(estimateReq.status).bg} rounded-full opacity-50`}
-                            ></span>
-                            <span className={`relative ${getStatusStyle(estimateReq.status).text}`}>
-                              {getStatusText(estimateReq.status)}
-                            </span>
-                          </span>
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          {estimateReq.status == 1 && (
-                            <button
-                              onClick={() => handleDelete(estimateReq._id)}
-                              className="text-indigo-600 hover:text-indigo-900"
-                            >
-                              삭제
-                            </button>
+        <div className="mt-8 flow-root">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">요청자</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">이사 날짜</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">출발지</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">도착지</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">상세 내용</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">금액</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">상태</th>
+                      <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                        <span className="sr-only">삭제</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {filteredEstimates.length > 0 ? (
+                      filteredEstimates.map((estimateReq) => (
+                        <tr key={estimateReq._id}>
+                          {estimateReq.request && (
+                            <>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6">
+                                {estimateReq.request.user_name || 'N/A'}
+                              </td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                                {estimateReq.request.move_date
+                                  ? new Date(estimateReq.request.move_date).toLocaleDateString()
+                                  : 'N/A'}
+                              </td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                                {estimateReq.request.start_address || 'N/A'}
+                              </td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                                {estimateReq.request.arrive_address || 'N/A'}
+                              </td>
+                            </>
                           )}
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                            {estimateReq.details || 'N/A'}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                            {estimateReq.amount ? `${estimateReq.amount}원` : 'N/A'}
+                          </td>
+                          <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <span className="relative inline-block px-3 py-1 font-semibold leading-tight">
+                              <span
+                                aria-hidden="true"
+                                className={`absolute inset-0 ${getStatusStyle(estimateReq.status).bg} rounded-full opacity-50`}
+                              ></span>
+                              <span className={`relative ${getStatusStyle(estimateReq.status).text}`}>
+                                {getStatusText(estimateReq.status)}
+                              </span>
+                            </span>
+                          </td>
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                            {estimateReq.status == 1 && (
+                              <button
+                                onClick={() => handleDelete(estimateReq._id)}
+                                className="text-indigo-600 hover:text-indigo-900"
+                              >
+                                삭제
+                              </button>
+                            )}
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan="8" className="text-center py-4 text-sm text-gray-500">
+                          요청서가 없습니다.
                         </td>
                       </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="8" className="text-center py-4 text-sm text-gray-500">
-                        요청서가 없습니다.
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+                    )}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
