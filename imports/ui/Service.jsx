5d5b8d0b071a0dc458516f2931e1ae416411ddae
hyojung222 <sxz0905@naver.com>
2024-12-17 13:23:58 +0900

    12/1 오후1시23분

--- imports/ui/Service.jsx
+++ imports/ui/Service.jsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div class='flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-pink-400 via-purple-300 to-indigo-400 '>
       <div class='w-full flex justify-center'>
-        <img src='/truckfix.png' alt='서비스이미지' class="w-[600px] h-[300px] " />
+        <img src='/truckfix.png' alt='서비스이미지' class="w-[400px] h-[300px] " />
       </div>
       <div class="flex flex-col items-center justify-center text-center w-[800px] h-[200px]">
         <span class="font-bold text-lg">소소이사에 오신걸 환영합니다!</span>
