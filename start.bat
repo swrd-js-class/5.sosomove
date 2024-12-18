be7c4c7b218b530ebf0b8c0d32834e630c761743
hyojung222 <sxz0905@naver.com>
2024-12-18 16:41:19 +0900

    12/18

--- start.bat
+++ start.bat
@@ -1,16 +1,11 @@
 @echo off
-@REM set AZURE_API_URL=https://shj-pk.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview
-@REM set MONGO_URL=mongodb+srv://ssong1001:songhe!0@cluster0.gwbeq.mongodb.net/?retryWrites=true^&w=majority^&appName=Cluster0
-@REM set AZURE_CV_URL=https://shj-cv.cognitiveservices.azure.com/vision/v3.2/analyze
-@REM set AZURE_CV_KEY=3bCGjL1DVZWqmm28yT2y3aHTPNYkleql49sRVFF5TIKsfkiBpT1KJQQJ99ALACYeBjFXJ3w3AAAFACOGUhdT
-@REM set AZURE_API_KEY=8FJ4HK4kROZM2zu1yhxQe3C5sOBMZZHCFjRT8jRTvxTy5L4g4uqgJQQJ99AKACYeBjFXJ3w3AAABACOGBbVs
+
+set MONGO_URL=mongodb://13.125.73.183:28888/sosomove
 
 set AZURE_API_URL=https://ai-sosogpt.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview
 set AZURE_API_KEY=9Z38qzJ5LwWvnTGgDrl6uZFGkRm60uVz6KhcawHHK7enymVHkMF9JQQJ99ALACYeBjFXJ3w3AAABACOGrqQp
 set AZURE_CV_KEY=6w72WwdKKMxVEZTZxLVha640eG50A1mekHsnePNuyLcphmXkHX5eJQQJ99ALACYeBjFXJ3w3AAAFACOG4dQ8
 set AZURE_CV_URL=https://ai-sosocv.cognitiveservices.azure.com/vision/v3.2/analyze
 
-
-set MONGO_URL=mongodb://13.125.73.183:28888/sosomove
 
 meteor run
