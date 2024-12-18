24a712a4ad3ccf0d420d174caf2c8a9a98da806b
Songhui <skyblue1001@naver.com>
2024-12-18 16:19:44 +0900

    파일 경로 수정

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-export MONGO_URL='mongodb+srv://ssong1001:songhe!0@cluster0.gwbeq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
-export AZURE_API_KEY = '8FJ4HK4kROZM2zu1yhxQe3C5sOBMZZHCFjRT8jRTvxTy5L4g4uqgJQQJ99AKACYeBjFXJ3w3AAABACOGBbVs';
-export AZURE_API_URL = 'https://shj-pk.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview';
-export AZURE_CV_KEY='3bCGjL1DVZWqmm28yT2y3aHTPNYkleql49sRVFF5TIKsfkiBpT1KJQQJ99ALACYeBjFXJ3w3AAAFACOGUhdT';
-export AZURE_CV_URL = 'https://shj-cv.cognitiveservices.azure.com/vision/v3.2/analyze';
+export MONGO_URL='mongodb://13.125.73.183:28888/sosomove'
+export AZURE_API_KEY = '9Z38qzJ5LwWvnTGgDrl6uZFGkRm60uVz6KhcawHHK7enymVHkMF9JQQJ99ALACYeBjFXJ3w3AAABACOGrqQp'
+export AZURE_API_URL = 'https://ai-sosogpt.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview'
+export AZURE_CV_KEY='6w72WwdKKMxVEZTZxLVha640eG50A1mekHsnePNuyLcphmXkHX5eJQQJ99ALACYeBjFXJ3w3AAAFACOG4dQ8'
+export AZURE_CV_URL = 'https://ai-sosocv.cognitiveservices.azure.com/vision/v3.2/analyze'
+
+export ROOT_URL="https://sosomove.site/"
 
 meteor run
