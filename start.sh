be9c620c67d0c406e422ae2b5d18eb67e0a647ad
kakadais <kakadais@gmail.com>
2025-01-02 03:47:16 +0900

    .

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
-export MONGO_URL='mongodb://13.125.73.183:28888/sosomove'
-export AZURE_API_KEY = '9Z38qzJ5LwWvnTGgDrl6uZFGkRm60uVz6KhcawHHK7enymVHkMF9JQQJ99ALACYeBjFXJ3w3AAABACOGrqQp'
-export AZURE_API_URL = 'https://ai-sosogpt.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview'
-export AZURE_CV_KEY='6w72WwdKKMxVEZTZxLVha640eG50A1mekHsnePNuyLcphmXkHX5eJQQJ99ALACYeBjFXJ3w3AAAFACOG4dQ8'
-export AZURE_CV_URL = 'https://ai-sosocv.cognitiveservices.azure.com/vision/v3.2/analyze'
+# export MONGO_URL='mongodb://13.125.73.183:28888/sosomove'
+# export AZURE_API_KEY='9Z38qzJ5LwWvnTGgDrl6uZFGkRm60uVz6KhcawHHK7enymVHkMF9JQQJ99ALACYeBjFXJ3w3AAABACOGrqQp'
+# export AZURE_API_URL='https://ai-sosogpt.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview'
+# export AZURE_CV_KEY='6w72WwdKKMxVEZTZxLVha640eG50A1mekHsnePNuyLcphmXkHX5eJQQJ99ALACYeBjFXJ3w3AAAFACOG4dQ8'
+# export AZURE_CV_URL='https://ai-sosocv.cognitiveservices.azure.com/vision/v3.2/analyze'
+export AZURE_API_URL=""
+export AZURE_API_KEY=""
+export AZURE_CV_KEY=""
+export AZURE_CV_URL=""
 
-export ROOT_URL="https://sosomove.site/"
+# export ROOT_URL="https://sosomove.site/"
 
 meteor run
